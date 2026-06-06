f46182afcf80dc955c0b8067346a96f4516e39a4
Mohammed Nibrasul Haqq <nibrasulhaqqamini@gmail.com>
2026-06-06 12:58:51 +0530

    Move nfc folder to WEB/public/nfc and update server.js static routing

--- WEB/server.js
+++ WEB/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5005;
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use('/nfc', express.static(path.join(__dirname, '../nfc')));
+app.use('/nfc', express.static(path.join(__dirname, 'public/nfc')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Database Layer State
